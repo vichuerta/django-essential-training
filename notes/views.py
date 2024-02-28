b6632b7e10d85c7646884a1c28d2638a1e07f850
Victor Huerta <vichuerta96@gmail.com>
2024-02-27 23:48:08 -0800

    refactor to class-based views

--- notes/views.py
+++ notes/views.py
@@ -1,16 +1,16 @@
 from django.shortcuts import render
 from django.http import Http404
+from django.views.generic import DetailView, ListView
 
 from .models import Notes
 
-def list(request):
-    all_notes = Notes.objects.all()
-    return render(request, 'notes/notes_list.html', {'notes': all_notes})
+class NotesListView(ListView):
+    model = Notes
+    # object to be passed into template
+    context_object_name = 'notes'
 
-def detail(request, pk):
-    try:
-        note = Notes.objects.get(pk=pk)
-    except Notes.DoesNotExist:
-        raise Http404("Note does not exist")
-    return render(request, 'notes/notes_detail.html', {'note': note})
+# handles exception already for non-existing notes
+class NotesDetailView(DetailView):
+    model = Notes
+    context_object_name = 'note'
 
