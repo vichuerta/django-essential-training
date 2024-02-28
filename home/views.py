2edf0559c820d05849306410c6dfa0bd308e61be
Victor Huerta <vichuerta96@gmail.com>
2024-02-27 19:19:07 -0800

    add authorized endpoint with redirect to admin page if not admin user

--- home/views.py
+++ home/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+from django.contrib.auth.decorators import login_required
 
 # every time this function recieves a request, it will return this http response
 
 # how does it know when to run? URLs file!
 def home(request):
     return render(request, 'home/welcome.html', {'today': datetime.today()})
+
+@login_required(login_url='/admin')
+def authorized(request):
+    return render(request,'home/authorized.html', {})
