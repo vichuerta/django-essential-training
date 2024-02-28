1630b08d8d251540fb0439fc3b09ec8b161e97c2
Victor Huerta <vichuerta96@gmail.com>
2024-02-27 23:28:04 -0800

    intro to Django class-based views

--- home/views.py
+++ home/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
-from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
-# every time this function recieves a request, it will return this http response
+# class-based view
+class HomeView(TemplateView):
+    template_name = 'home/welcome.html'
+    extra_context = {'today': datetime.today()}
 
-# how does it know when to run? URLs file!
-def home(request):
-    return render(request, 'home/welcome.html', {'today': datetime.today()})
-
-@login_required(login_url='/admin')
-def authorized(request):
-    return render(request,'home/authorized.html', {})
+class AuthorizedView(LoginRequiredMixin, TemplateView):
+    template_name = 'home/authorized.html'
+    login_url = '/admin'
