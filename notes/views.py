38c173fb879f23f6696c8e479a5ecf9eee3bb8cd
Victor Huerta <vichuerta96@gmail.com>
2024-03-10 16:24:44 -0700

    create method for notes view

--- notes/views.py
+++ notes/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import Http404
-from django.views.generic import DetailView, ListView
+from django.views.generic import CreateView, DetailView, ListView
 
 from .models import Notes
+
+class NotesCreateView(CreateView):
+    # what the endpoint is regarding
+    model = Notes
+    # attributes of the model
+    fields = ['title', 'text']
+    # redirect the user to the list of created/existing notes
+    success_url = '/smart/notes'
 
 class NotesListView(ListView):
     model = Notes
