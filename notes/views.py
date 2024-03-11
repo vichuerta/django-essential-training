d71be447c6aed0fbf7d481b0591f4b9857e4016d
Victor Huerta <vichuerta96@gmail.com>
2024-03-10 17:18:24 -0700

    Django forms: Powerful validation with minimal work

--- notes/views.py
+++ notes/views.py
@@ -2,15 +2,15 @@ from django.shortcuts import render
 from django.http import Http404
 from django.views.generic import CreateView, DetailView, ListView
 
+from .forms import NotesForm
 from .models import Notes
 
 class NotesCreateView(CreateView):
     # what the endpoint is regarding
     model = Notes
-    # attributes of the model
-    fields = ['title', 'text']
     # redirect the user to the list of created/existing notes
     success_url = '/smart/notes'
+    form_class = NotesForm
 
 class NotesListView(ListView):
     model = Notes
