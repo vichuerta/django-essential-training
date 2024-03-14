11b734dbbc2b8fd02a9d90620e7749a8431cf995
Victor Huerta <vichuerta96@gmail.com>
2024-03-13 19:29:12 -0700

    The D in the CRUD: Deleting data

--- notes/views.py
+++ notes/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import Http404
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
+from django.views.generic.edit import DeleteView
 
 from .forms import NotesForm
 from .models import Notes
@@ -27,3 +28,8 @@ class NotesUpdateView(UpdateView):
     # redirect the user to the list of created/existing notes
     success_url = '/smart/notes'
     form_class = NotesForm
+
+class NotesDeleteView(DeleteView):
+    model = Notes
+    success_url = '/smart/notes'
+    template_name = 'notes/notes_delete.html'
