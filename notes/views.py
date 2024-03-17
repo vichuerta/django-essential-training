9cf37704044194ee336fe6b4e38e83f963fbb05a
Victor Huerta <vichuerta96@gmail.com>
2024-03-17 13:09:29 -0700

    Adding a new note after ForeignKey - add LoginRequiredMixin to views - inject logged in user to create view

--- notes/views.py
+++ notes/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponseRedirect
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
 from django.views.generic.edit import DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -5,12 +6,20 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import NotesForm
 from .models import Notes
 
-class NotesCreateView(CreateView):
+class NotesCreateView(LoginRequiredMixin, CreateView):
     # what the endpoint is regarding
     model = Notes
     # redirect the user to the list of created/existing notes
     success_url = '/smart/notes'
     form_class = NotesForm
+    login_url = '/admin'
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        # inject the logged user
+        self.object.user = self.request.user
+        self.object.save()
+        return HttpResponseRedirect(self.get_success_url())
 
 class NotesListView(LoginRequiredMixin, ListView):
     model = Notes
@@ -24,17 +33,20 @@ class NotesListView(LoginRequiredMixin, ListView):
         return self.request.user.notes.all()
 
 # handles exception already for non-existing notes
-class NotesDetailView(DetailView):
+class NotesDetailView(LoginRequiredMixin, DetailView):
     model = Notes
     context_object_name = 'note'
+    login_url = '/admin'
 
-class NotesUpdateView(UpdateView):
+class NotesUpdateView(LoginRequiredMixin, UpdateView):
     model = Notes
     # redirect the user to the list of created/existing notes
     success_url = '/smart/notes'
     form_class = NotesForm
+    login_url = '/admin'
 
-class NotesDeleteView(DeleteView):
+class NotesDeleteView(LoginRequiredMixin, DeleteView):
     model = Notes
     success_url = '/smart/notes'
     template_name = 'notes/notes_delete.html'
+    login_url = '/admin'
