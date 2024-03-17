6b591175f186cb94d96d1548e457ed3d0ee454e6
Victor Huerta <vichuerta96@gmail.com>
2024-03-17 14:05:41 -0700

    Adding login and logout pages

--- home/views.py
+++ home/views.py
@@ -3,12 +3,15 @@ from django.http import HttpResponse
 from datetime import datetime
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.views import LoginView, LogoutView
+
+class LoginInterfaceView(LoginView):
+    template_name = 'home/login.html'
+
+class LogoutInterfaceView(LogoutView):
+    template_name = 'home/logout.html'
 
 # class-based view
 class HomeView(TemplateView):
     template_name = 'home/welcome.html'
     extra_context = {'today': datetime.today()}
-
-class AuthorizedView(LoginRequiredMixin, TemplateView):
-    template_name = 'home/authorized.html'
-    login_url = '/admin'
