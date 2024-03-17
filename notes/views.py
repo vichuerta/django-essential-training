664bb4fd98c9332420ee99c64ed3bed50b87e1be
Victor Huerta <vichuerta96@gmail.com>
2024-03-17 13:01:13 -0700

    Displaying only the logged user data

--- notes/views.py
+++ notes/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
-from django.http import Http404
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
 from django.views.generic.edit import DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import NotesForm
 from .models import Notes
@@ -13,10 +12,16 @@ class NotesCreateView(CreateView):
     success_url = '/smart/notes'
     form_class = NotesForm
 
-class NotesListView(ListView):
+class NotesListView(LoginRequiredMixin, ListView):
     model = Notes
     # object to be passed into template
     context_object_name = 'notes'
+    template_name = 'notes/notes_list.html'
+    # if a user tries to access the list view and it's not logged in, they will be redirected to this page instead of seeing a 404
+    login_url = '/admin'
+
+    def get_queryset(self):
+        return self.request.user.notes.all()
 
 # handles exception already for non-existing notes
 class NotesDetailView(DetailView):
